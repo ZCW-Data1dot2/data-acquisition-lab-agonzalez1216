3bd5cf010a99047ee1cfdc3c8fc4f9ccb19907b8
alfonso <alfonso@zipcodes-mbp.myfiosgateway.com>
2020-11-18 12:42:06 -0500

    first commit -AG

---
+++ make_requests.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+i = 0
+offset = 1
+limit = 1000
+headers = {'Token': 'iPUFLyvvyzixkBcMRarLLeoLASMlWkEE'
+           }
+payload = {'limit': limit,
+           'offset': offset}
+data_set_name = 'locations'
+r = requests.get(f'https://www.ncdc.noaa.gov/cdo-web/api/v2/{data_set_name}', headers=headers, params=payload)
+r_dict = r.json()
+total_results = r_dict['metadata']['resultset']['count']
+r_str = json.dumps(r_dict, indent=4)
+while offset <= total_results:
+    with open(f'{data_set_name}_{i}.json', 'w') as outfile:
+        outfile.write(r_str)
+    i += 1
+    offset = offset + limit
+    payload = {'limit': limit,
+               'offset': offset}
+
+    r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets', headers=headers, params=payload)
+    r_dict = r.json()
+    r_str = json.dumps(r_dict, indent=4)
