03782c93bdd3f3a86941f56a59a7e4f75108fcf5
alfonso <alfonso@zipcodes-mbp.myfiosgateway.com>
2020-11-19 12:52:30 -0500

    final -AG

--- make_requests.py
+++ make_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 i = 0
 offset = 1
@@ -14,13 +15,13 @@ r_dict = r.json()
 total_results = r_dict['metadata']['resultset']['count']
 r_str = json.dumps(r_dict, indent=4)
 while offset <= total_results:
+    time.sleep(.75)
     with open(f'{data_set_name}_{i}.json', 'w') as outfile:
         outfile.write(r_str)
     i += 1
     offset = offset + limit
     payload = {'limit': limit,
                'offset': offset}
-
-    r = requests.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets', headers=headers, params=payload)
+    r = requests.get(f'https://www.ncdc.noaa.gov/cdo-web/api/v2/{data_set_name}', headers=headers, params=payload)
     r_dict = r.json()
     r_str = json.dumps(r_dict, indent=4)
